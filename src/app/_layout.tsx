3a368412c7714090a0a131d65e7f6d9225a0813c
Aditya Kumar Soni <soniadityakumar651@gmail.com>
2026-03-02 21:09:02 +0530

    Sentry setup is done the tabs navigation is also done to the chat screen , explore screen and profile screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,12 +3,37 @@ import "../../global.css";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
+import * as Sentry from "@sentry/react-native";
+
+Sentry.init({
+  dsn: "https://00c29f8afaf7987d3f83d2857e97423f@o4510975572115456.ingest.de.sentry.io/4510975581225041",
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
 export default function RootLayout() {
   return (
     // This token is to keep the info of the user even if they close the app.
     <ClerkProvider tokenCache={tokenCache}>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(tabs)" />
       </Stack>
     </ClerkProvider>
   );
